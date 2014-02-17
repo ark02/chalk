49895c7c9d235def0474f76da855f1695b46b09a
Amir Khazaieli <ark02@umail.ucsb.edu>
2014-02-17 00:40:28 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chalk::Application.config.secret_key_base = '7d3dbf445605ebe5242d38b900a44dba20e015e69e66b02c051e801288a6f58327cfb79d864495f68c7ca97f8d76759a86a50cda6471a9c8f4517a9a5b577c24'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chalk::Application.config.session_store :cookie_store, key: '_chalk_session'
